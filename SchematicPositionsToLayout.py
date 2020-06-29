8f214207456cbcb5dbce0a567419e21d008a59fc
Ian Ross <ian@skybluetrades.net>
2020-06-29 22:05:50 +0200

    Fix layout for hierarchical sheets

--- SchematicPositionsToLayout.py
+++ SchematicPositionsToLayout.py
@@ -4,6 +4,8 @@ import re
 import sys
 import pcbnew
 
+DEBUG = None
+
 # Tokenizer for schematic file input lines.
 def tokens(s):
     return re.split(r' +', s)
@@ -30,7 +32,7 @@ class SchSheet:
         self.components = dict()
         self.sub_sheets = dict()
 
-        print('New sheet from:', file)
+        print('New sheet from:', file, file=DEBUG)
 
         self.xrange = [None, None]
         self.yrange = [None, None]
@@ -39,8 +41,10 @@ class SchSheet:
             with open(file) as fp:
                 state = None
                 component_ref = None
+                component_id = None
                 component_pos = None
                 sheet_name = None
+                sheet_id = None
                 sheet_file = None
                 sheet_bounds = None
                 for line in fp:
@@ -53,34 +57,44 @@ class SchSheet:
                     if line == '$EndComp':
                         state = None
                         component_ref = None
+                        component_id = None
                         component_pos = None
                     if line == '$Sheet':
                         state = 'in-sheet'
                     if line == '$EndSheet':
                         state = None
                         sheet_name = None
+                        sheet_id = None
                         sheet_file = None
+                        sheet_bounds = None
 
                     # Handle component lines.
                     if state == 'in-component':
                         if line.startswith('L '):
                             component_ref = tokens(line)[2]
+                        if line.startswith('U '):
+                            component_id = tokens(line)[3]
                         if line.startswith('P '):
                             component_pos = tuple(map(int, tokens(line)[1:]))
                         # If we have a component reference and a
                         # component position, record them and include
                         # the component position in the coordinate
                         # ranges.
-                        if component_ref is not None and component_pos is not None:
-                            self.components[component_ref] = component_pos
+                        if (component_ref is not None and
+                            component_id is not None and
+                            component_pos is not None):
+                            self.components[component_id] = (component_ref, component_pos)
                             self.extend_range(component_pos[0], component_pos[1])
                             component_ref = None
+                            component_id = None
                             component_pos = None
 
                     # Handle sub-sheet lines.
                     if state == 'in-sheet':
                         if line.startswith('S '):
                             sheet_bounds = tuple(map(int, tokens(line)[1:]))
+                        if line.startswith('U '):
+                            sheet_id = tokens(line)[1]
                         if line.startswith('F0 '):
                             sheet_name = line.split('"')[1]
                         if line.startswith('F1 '):
@@ -89,29 +103,35 @@ class SchSheet:
                         # sheet bounds, record them and include the
                         # bounds in the coordinate ranges.
                         if (sheet_name is not None and
+                            sheet_id is not None and
                             sheet_file is not None and
                             sheet_bounds is not None):
-                            self.sub_sheets[sheet_name] = sheet_file
+                            self.sub_sheets[sheet_id] = (sheet_name, sheet_file)
                             self.extend_range(sheet_bounds[0], sheet_bounds[1])
                             self.extend_range(sheet_bounds[0] + sheet_bounds[2],
                                               sheet_bounds[1] + sheet_bounds[3])
                             sheet_name = None
+                            sheet_id = None
                             sheet_file = None
                             sheet_bounds = None
 
         except Exception as err:
-            print('Failed reading schematic file:', err)
+            print('Failed reading schematic file:', err, file=DEBUG)
             sys.exit(1)
 
 POS_SCALE = 15000
 
-def move_modules(components, board, xsize, ysize):
+def move_modules(components, board, offsets):
     for module in board.GetModules():
         old_pos = module.GetPosition()
         ref = module.GetReference()
-        if ref in components:
-            pos, sheet, idx = components[ref]
-            new_pos = pcbnew.wxPoint(pos[0] * POS_SCALE, (pos[1] + idx * ysize) * POS_SCALE)
+        path = module.GetPath()
+        if path in components:
+            ref, pos, sheet = components[path]
+            offset = offsets[sheet]
+            new_pos = pcbnew.wxPoint(pos[0] * POS_SCALE, (pos[1] + offset) * POS_SCALE)
+            print('path =', path, '  sheet =', sheet, '  ref =', ref,
+                  '  pos =', pos, '  new_pos =', new_pos, file=DEBUG)
             module.SetPosition(new_pos)
 
 class SchematicPositionsToLayoutPlugin(pcbnew.ActionPlugin):
@@ -121,55 +141,68 @@ class SchematicPositionsToLayoutPlugin(pcbnew.ActionPlugin):
         self.description = "Layout components on PCB in same spatial relationships as components on schematic"
 
     def Run(self):
+        global DEBUG
+        DEBUG = open('schematic-positions-to-layout.debug', 'w')
+        try:
+            self.DoRun()
+        finally:
+            DEBUG.close()
+
+    def DoRun(self):
         board = pcbnew.GetBoard()
         work_dir, in_pcb_file = os.path.split(board.GetFileName())
         os.chdir(work_dir)
         root_schematic_file = os.path.splitext(in_pcb_file)[0] + '.sch'
-        print('work_dir = {}'.format(work_dir), file=sys.stderr)
-        print('in_pcb_file = {}'.format(in_pcb_file), file=sys.stderr)
-        print('root_schematic_file = {}'.format(root_schematic_file), file=sys.stderr)
+        print('work_dir = {}'.format(work_dir), file=DEBUG)
+        print('in_pcb_file = {}'.format(in_pcb_file), file=DEBUG)
+        print('root_schematic_file = {}'.format(root_schematic_file), file=DEBUG)
 
         # Read schematic sheets, starting at root sheet and following
         # links to sub-sheets.
         sheets = dict()
         sheet_queue = dict()
-        sheet_queue['/'] = root_schematic_file
+        sheet_queue[''] = ('', root_schematic_file)
         while len(sheet_queue) > 0:
-            sheet_name = list(sheet_queue)[0]
-            if sheet_name in sheets:
-                print('Oops. Sheet "{}" turned up twice!'.format(sheet_name))
+            sheet_path = list(sheet_queue)[0]
+            if sheet_path in sheets:
+                print('Oops. Sheet "{}" turned up twice!'.format(sheet_path), file=DEBUG)
                 sys.exit(1)
-            file_name = sheet_queue.pop(sheet_name)
+            sheet_name, file_name = sheet_queue.pop(sheet_path)
             sheet = SchSheet(file_name)
-            sheets[sheet_name] = sheet
+            sheets[sheet_path] = sheet
             for sub_sheet_name in sheet.sub_sheets:
-                sheet_queue[sub_sheet_name] = sheet.sub_sheets[sub_sheet_name]
+                sheet_queue[sheet_path + '/' + sub_sheet_name] = sheet.sub_sheets[sub_sheet_name]
 
-        # Find maximum coordinate range across all sheets. We'll use this
-        # for the size of each sub-sheet in the layout.
-        xsize = sheets['/'].xrange[1] - sheets['/'].xrange[0]
-        ysize = sheets['/'].yrange[1] - sheets['/'].yrange[0]
+        # Find coordinate offsets for placement of each sub-sheet in
+        # the layout.
+        offsets = dict()
+        offsets[''] = 0
+        running_offset = sheets[''].yrange[1] - sheets[''].yrange[0]
         for sname in sheets:
-            s = sheets[sname]
-            xsize = max(xsize, s.xrange[1] - s.xrange[0])
-            ysize = max(ysize, s.yrange[1] - s.yrange[0])
-
-        # Make a master component map, recording the component's position
-        # in the sheet and the index of the sheet that it's on.
-        components = dict()
-        s = sheets['/']
-        for cid in s.components:
-            components[cid] = (s.components[cid], '/', 0)
-        index = 1
-        for sheet_name in sheets:
-            if sheet_name == '/':
+            if sname == '':
                 continue
+            s = sheets[sname]
+            offsets[sname] = running_offset
+            running_offset += s.yrange[1] - s.yrange[0]
+
+        # Make a master component map, recording the component's
+        # position in the sheet.
+        components = dict()
+        s = sheets['']
+        for cid in s.components:
+            components[cid] = s.components[cid] + tuple([''])
+        for sheet_name in sheets:
+            if sheet_name == '':
+                continue
+            print('Processing', sheet_name, file=DEBUG)
             s = sheets[sheet_name]
             for cid in s.components:
-                components[cid] = (s.components[cid], sheet_name, index)
-            index += 1
+                components[sheet_name + '/' + cid] = s.components[cid] + tuple([sheet_name])
+
+        for cid in components:
+            print(cid, components[cid], file=DEBUG)
 
         # Move the components.
-        move_modules(components, board, xsize, ysize)
+        move_modules(components, board, offsets)
 
 SchematicPositionsToLayoutPlugin().register()
